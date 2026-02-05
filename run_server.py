0aefa703d07dd3124753e5ed9fe42740e4745692
yashxog <sonaryash70@gmail.com>
2026-02-05 09:41:41 +0530

    FIX: deployment error fixed.

--- run_server.py
+++ run_server.py
@@ -1,8 +1,9 @@
 import uvicorn
-from app.server import create_app
+import os
+from app.server import create_app   
 
 app = create_app()
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Use Render's PORT, fallback to 8000 for local dev
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
